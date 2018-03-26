cdac9406908ec4e1d36466cc6972d18c7d7b0e0d
unknown <ericblarson125@gmail.com>
2018-03-25 22:48:07 -0700

    Condensed python script into single function for finding the race names

--- sx_race_prediction.py
+++ sx_race_prediction.py
@@ -14,26 +14,36 @@ import sys
 from bs4 import BeautifulSoup
 import urllib
 
-#gets the ama archives website
-response = requests.get("https://archives.amasupercross.com/")
-html = response.text
-soup = BeautifulSoup(html, "html.parser")
+def store_races(db):
+	# accesses amasupercross.com and parsers the website to read the race locations/names
+	# stores the race names into database for future reference
+	# db is the object for the database retrieved in main
+	response = requests.get("https://archives.amasupercross.com/")
+	html = response.text
+	soup = BeautifulSoup(html, "html.parser")
 
-# grabs iframe from archive website and 
-# joins the iframe url to main url
-iframe = soup.find(id = "ifrm")["src"]
-iframe_url = urllib.parse.urljoin("https://archives.amasupercross.com/", iframe)
+	# grabs iframe from archive website and joins the iframe url to main url
+	iframe = soup.find(id = "ifrm")["src"]
+	iframe_url = urllib.parse.urljoin("https://archives.amasupercross.com/", iframe)
 
-# creates new request and soup for the iframe request
-iframe_response = urllib.request.urlopen(iframe_url)
-iframe_soup = BeautifulSoup(iframe_response, "html.parser")
+	# creates new request and soup for the iframe portion of webpage
+	iframe_response = urllib.request.urlopen(iframe_url)
+	iframe_soup = BeautifulSoup(iframe_response, "html.parser")
 
-# gets the tables from the iframe
-tables = iframe_soup.find_all("table")
-# narrows the tables to be just the one from 2018-2015
-table = tables[1]
-rows = table.find_all("tr")
-#removes the first row because it is just formatting
-rows.pop(0)
+	# gets the tables from the iframe and selects the one for years 2018-2015
+	tables = iframe_soup.find_all("table")
+	table = tables[1]
+
+	#gets the rows of the table and removes the first one which is just formatting
+	rows = table.find_all("tr")	
+	rows.pop(0)
+	# gets the string inside the anchor for the race name
+	# saves string into the database
+	for row in rows:
+		race = rows[0].find("td").a.contents[0]
+		db.races.insert({"name": race})
+
+# this line will parse the anchor href with the main url
+print (urllib.parse.urljoin("https://archives.amasupercross.com/", race))
 
 
